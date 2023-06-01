e03d906588507e02271673a84905125cd78c4bc7
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-01 15:07:37 -0300

    Removing unnecessary import on migration 2023_06_01_145844_drop_personal_access_tokens_table.

--- database/migrations/2023_06_01_145844_drop_personal_access_tokens_table.php
+++ database/migrations/2023_06_01_145844_drop_personal_access_tokens_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Schema as Schema;
 use Illuminate\Database\Migrations\Migration as Migration;
-use Illuminate\Database\Schema\Blueprint as Blueprint;
 
 return new class extends Migration {
 
