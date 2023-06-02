4a480cd19e12943915ddc9a5455d2c68d4cd1fa7
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-02 13:53:13 -0300

    Creation of relationship between models of authentication tokens and bearer tokens.

--- app/Models/AuthToken.php
+++ app/Models/AuthToken.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model as Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo as BelongsTo;
 use App\Models\User as User;
+use Illuminate\Database\Eloquent\Relations\HasOne as HasOne;
+use App\Models\BearerToken as BearerToken;
 
 class AuthToken extends Model {
 
@@ -15,6 +17,12 @@ class AuthToken extends Model {
     public function user(): BelongsTo {
         
         return $this->belongsTo(User::class, 'user', 'id');
+
+    }
+
+    public function bearerTokens(): HasOne {
+        
+        return $this->hasOne(BearerToken::class, 'auth_token', 'id');
 
     }
 
