a58835689a7dbe0ec5c8e74212888807da4a984e
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-02 00:25:33 -0300

    The action responsible for registering new authentication tokens was protected against exceptions.

--- app/Http/Controllers/API/AuthTokenController.php
+++ app/Http/Controllers/API/AuthTokenController.php
@@ -8,6 +8,7 @@ use App\Models\User as User;
 use App\Traits\TokenTrait as TokenTrait;
 use App\Models\AuthToken as AuthToken;
 use App\Http\Requests\AuthTokenRequest as AuthTokenRequest;
+use \Exception as Exception;
 
 class AuthTokenController extends Controller {
 
@@ -15,22 +16,30 @@ class AuthTokenController extends Controller {
 
     public function store(AuthTokenRequest $request): JsonResponse {
 
-        $user = User::where('email', '=', $request->email)->first();
+        try {
+            
+            $user = User::where('email', '=', $request->email)->first();
+            
+            if ($user == null) {
+    
+                $user = new User();
+                $user->email = $request->email;
+                $user->save();
+    
+            }
+    
+            $authToken = new AuthToken();
+            $authToken->auth_token = $this->generateToken(config('auth.auth_token_size'));
+            $authToken->user = $user->id;
+            $authToken->save();
+    
+            dd($authToken);
 
-        if ($user == null) {
+        } catch (Exception $exception) {
 
-            $user = new User();
-            $user->email = $request->email;
-            $user->save();
+            dd($exception);
 
         }
-
-        $authToken = new AuthToken();
-        $authToken->auth_token = $this->generateToken(config('auth.auth_token_size'));
-        $authToken->user = $user->id;
-        $authToken->save();
-
-        dd($authToken);
 
     }
 
