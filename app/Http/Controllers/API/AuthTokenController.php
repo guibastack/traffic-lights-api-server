f1aac55310038aa00816b6a14276c63841497459
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-02 00:43:19 -0300

    The authentication token generation action now responds in JSON format.

--- app/Http/Controllers/API/AuthTokenController.php
+++ app/Http/Controllers/API/AuthTokenController.php
@@ -9,10 +9,11 @@ use App\Traits\TokenTrait as TokenTrait;
 use App\Models\AuthToken as AuthToken;
 use App\Http\Requests\AuthTokenRequest as AuthTokenRequest;
 use \Exception as Exception;
+use App\Traits\ResponseTrait as ResponseTrait;
 
 class AuthTokenController extends Controller {
 
-    use TokenTrait;
+    use TokenTrait, ResponseTrait;
 
     public function store(AuthTokenRequest $request): JsonResponse {
 
@@ -27,17 +28,19 @@ class AuthTokenController extends Controller {
                 $user->save();
     
             }
-    
+
             $authToken = new AuthToken();
             $authToken->auth_token = $this->generateToken(config('auth.auth_token_size'));
             $authToken->user = $user->id;
             $authToken->save();
-    
-            dd($authToken);
+            
+            return $this->responseInJSON(200, 'A token has been generated and sent to your email.', [
+                'email_provided' => $request->email,
+            ]);
 
         } catch (Exception $exception) {
-
-            dd($exception);
+            
+            return $this->responseInJSON(500, 'Internal Server Error. Try again later.', null);
 
         }
 
