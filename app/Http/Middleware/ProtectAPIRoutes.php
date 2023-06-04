684beda8e34697ea11ae97d0e8b343f64abc78f8
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-04 00:19:59 -0300

    API route protection middleware has been protected against unexpected exceptions.

--- app/Http/Middleware/ProtectAPIRoutes.php
+++ app/Http/Middleware/ProtectAPIRoutes.php
@@ -7,6 +7,7 @@ use \Closure as Closure;
 use Symfony\Component\HttpFoundation\Response as Response;
 use App\Traits\ResponseTrait as ResponseTrait;
 use App\Models\BearerToken as BearerToken;
+use \Exception as Exception;
 
 class ProtectAPIRoutes {
 
@@ -14,33 +15,41 @@ class ProtectAPIRoutes {
 
     public function handle(Request $request, Closure $next): Response {
         
-        if ($request->bearerToken() == null) {
+        try {
             
-            return $this->responseInJSON(401, 'There is no bearer token in the request.', [
-                'bearer_token_provided' => $request->bearerToken(),
-            ]);
+            if ($request->bearerToken() == null) {
+                
+                return $this->responseInJSON(401, 'There is no bearer token in the request.', [
+                    'bearer_token_provided' => $request->bearerToken(),
+                ]);
+    
+            }
+    
+            $bearerToken = BearerToken::where('bearer_token', '=', $request->bearerToken())->first();
+    
+            if ($bearerToken == null) {
+    
+                return $this->responseInJSON(401, 'The provided bearer token is not linked to any user account.', [
+                    'bearer_token_provided' => $request->bearerToken(),
+                ]);
+    
+            }
+    
+            if ($bearerToken->isExpired()) {
+    
+                return $this->responseInJSON(409, 'The provided bearer token is expired.', [
+                    'bearer_token_provided' => $request->bearerToken(),
+                ]);
+    
+            }
+    
+            return $next($request);
+            
+        } catch (Exception $e) {
+
+            return $this->responseInJSON(500, 'Internal Server Error. Try again later.', null);
 
         }
-
-        $bearerToken = BearerToken::where('bearer_token', '=', $request->bearerToken())->first();
-
-        if ($bearerToken == null) {
-
-            return $this->responseInJSON(401, 'The provided bearer token is not linked to any user account.', [
-                'bearer_token_provided' => $request->bearerToken(),
-            ]);
-
-        }
-
-        if ($bearerToken->isExpired()) {
-
-            return $this->responseInJSON(409, 'The provided bearer token is expired.', [
-                'bearer_token_provided' => $request->bearerToken(),
-            ]);
-
-        }
-
-        return $next($request);
 
     }
 
