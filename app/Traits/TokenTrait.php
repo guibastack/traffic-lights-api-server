8d9e6dfaa65c98c7a2103052f9f2f6a6a48ab1e4
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-04 13:33:08 -0300

    Creation of a mechanism for verifying the uniqueness of tokens. It is important that this function is decoupled from the token generation function, because in the future, not all tokens will not necessarily repeat.

--- app/Traits/TokenTrait.php
+++ app/Traits/TokenTrait.php
@@ -3,6 +3,7 @@
 namespace App\Traits;
 
 use \DateTime as DateTime;
+use Illuminate\Database\Eloquent\Model as Model;
 
 trait TokenTrait {
 
@@ -23,6 +24,30 @@ trait TokenTrait {
     public function calculateTokenExpiryTime(DateTime $tokenGenerationDateTime, int $tokenExpirationInSeconds): DateTime {
         
         return new DateTime(date('Y-m-d H:i:s', ($tokenGenerationDateTime->getTimestamp() + $tokenExpirationInSeconds)));
+
+    }
+
+    public function tokenIsUnique(Model $model, string $column, string $token, ?array $dataSet): bool {
+
+        $query = $model->where($column, '=', $token);
+
+        if ($dataSet != null) {
+
+            foreach ($dataSet as $key => $value) {
+                
+                $query->where($key, '=', $value);
+                
+            }
+
+        }
+
+        if (count($query->get()) > 0) {
+
+            return false;
+
+        }
+
+        return true;
 
     }
 
