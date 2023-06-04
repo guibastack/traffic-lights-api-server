54477f3555fbf343628747565380c1b756f0dab4
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-04 00:10:10 -0300

    Creation of API route protection middleware (using bearer token).

---
+++ app/Http/Middleware/ProtectAPIRoutes.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request as Request;
+use \Closure as Closure;
+use Symfony\Component\HttpFoundation\Response as Response;
+use App\Traits\ResponseTrait as ResponseTrait;
+use App\Models\BearerToken as BearerToken;
+
+class ProtectAPIRoutes {
+
+    use ResponseTrait;
+
+    public function handle(Request $request, Closure $next): Response {
+        
+        if ($request->bearerToken() == null) {
+            
+            return $this->responseInJSON(401, 'There is no bearer token in the request.', [
+                'bearer_token_provided' => $request->bearerToken(),
+            ]);
+
+        }
+
+        $bearerToken = BearerToken::where('bearer_token', '=', $request->bearerToken())->first();
+
+        if ($bearerToken == null) {
+
+            return $this->responseInJSON(401, 'The provided bearer token is not linked to any user account.', [
+                'bearer_token_provided' => $request->bearerToken(),
+            ]);
+
+        }
+
+        if ($bearerToken->isExpired()) {
+
+            return $this->responseInJSON(409, 'The provided bearer token is expired.', [
+                'bearer_token_provided' => $request->bearerToken(),
+            ]);
+
+        }
+
+        return $next($request);
+
+    }
+
+}
