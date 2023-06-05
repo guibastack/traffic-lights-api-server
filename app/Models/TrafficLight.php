cfb587932c8ac0147674d87b66b8876864459505
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-05 12:25:51 -0300

    Creation of a model (and associated table) of general information regarding traffic lights.

---
+++ app/Models/TrafficLight.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model as Model;
+
+class TrafficLight extends Model {
+
+    protected $table = 'traffic_lights';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+
+}
