24d46b19c1d96813b6083aeab242cb03b4beb0f8
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-07 14:30:02 -0300

    The 'store' action of the 'TrafficLightController' controller is now protected against unexpected exceptions.

--- app/Http/Controllers/API/TrafficLightController.php
+++ app/Http/Controllers/API/TrafficLightController.php
@@ -10,40 +10,49 @@ use App\Models\BearerToken as BearerToken;
 use App\Traits\ResponseTrait as ResponseTrait;
 use App\Http\Requests\TrafficLightRequest as TrafficLightRequest;
 use \DateTime as DateTime;
+use \Exception as Exception;
 
 class TrafficLightController extends Controller {
 
     use ResponseTrait;
 
     public function store(TrafficLightRequest $request): JsonResponse {
-        
-        $trafficLight = TrafficLight::where('latitude', '=', $request->latitude)
-                                    ->where('longitude', '=', $request->longitude)
-                                    ->first();
 
-        if ($trafficLight == null) {
+        try {
 
-            $trafficLight = new TrafficLight();
-            $trafficLight->latitude = $request->latitude;
-            $trafficLight->longitude = $request->longitude;
-            $trafficLight->save();
+            $trafficLight = TrafficLight::where('latitude', '=', $request->latitude)
+                                        ->where('longitude', '=', $request->longitude)
+                                        ->first();
+    
+            if ($trafficLight == null) {
+    
+                $trafficLight = new TrafficLight();
+                $trafficLight->latitude = $request->latitude;
+                $trafficLight->longitude = $request->longitude;
+                $trafficLight->save();
+    
+            }
+    
+            $user = (BearerToken::where('bearer_token', '=', $request->bearerToken())
+                                ->first())->authToken->ownerUser;
+    
+            $trafficLightHistoryItem = new TrafficLightHistoryItem();
+            $trafficLightHistoryItem->red_light_start = new DateTime($request->red_light_start);
+            $trafficLightHistoryItem->red_light_duration_in_seconds = $request->red_light_duration_in_seconds;
+            $trafficLightHistoryItem->yellow_light_duration_in_seconds = $request->yellow_light_duration_in_seconds;
+            $trafficLightHistoryItem->green_light_duration_in_seconds = $request->green_light_duration_in_seconds;
+            $trafficLightHistoryItem->traffic_light = $trafficLight->id;
+            $trafficLightHistoryItem->user = $user->id;
+            $trafficLightHistoryItem->name = $request->name;
+            $trafficLightHistoryItem->save();
+    
+            return $this->responseInJson(200, 'Traffic light successfully mapped.', null);
+
+        } catch (Exception $e) {
+            
+            return $this->responseInJSON(500, 'Internal Server Error. Try again later.', null);
 
         }
-
-        $user = (BearerToken::where('bearer_token', '=', $request->bearerToken())
-                            ->first())->authToken->ownerUser;
-
-        $trafficLightHistoryItem = new TrafficLightHistoryItem();
-        $trafficLightHistoryItem->red_light_start = new DateTime($request->red_light_start);
-        $trafficLightHistoryItem->red_light_duration_in_seconds = $request->red_light_duration_in_seconds;
-        $trafficLightHistoryItem->yellow_light_duration_in_seconds = $request->yellow_light_duration_in_seconds;
-        $trafficLightHistoryItem->green_light_duration_in_seconds = $request->green_light_duration_in_seconds;
-        $trafficLightHistoryItem->traffic_light = $trafficLight->id;
-        $trafficLightHistoryItem->user = $user->id;
-        $trafficLightHistoryItem->name = $request->name;
-        $trafficLightHistoryItem->save();
-
-        return $this->responseInJson(200, 'Traffic light successfully mapped.', null);
 
     }
 
